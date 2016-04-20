64b420502a264795c99b2932ce8345d0de1ebba8
sukergoal <songgemail@sina.com>
2016-04-20 17:48:16 +0800

    本地调用服务LOGO

---
+++ PythonTest/src/desktop/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@version:1.0
+@author: Suker
+@license:
+@contact:songgemail@sina.com
+@software: PyCharm
+@time: 2016/4/19 16:40
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
